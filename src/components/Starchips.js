e3e4dbd5fd8eff77ada63e39072bd2160af88618
Gabriel Guzman <gguzmanlockward@gmail.com>
2021-03-18 19:46:17 -0400

    addind layout

---
+++ src/components/Starchips.js
@@ -0,0 +1,18 @@
+import { memo } from "react";
+
+const Starchips = ({}) => {
+  return (
+    <table>
+      <thead>
+        <th>manufacturer</th>
+        <th>name</th>
+        <th>model</th>
+        <th>model</th>
+        <th>crew</th>
+        <th>passengers</th>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+
---
+++ src/containers/Layout.js
@@ -0,0 +1,65 @@
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
+  },
+}));
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Sticky footer
+        </Typography>
+        {children}
+      </Container>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            My sticky footer can be found here.
+          </Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
