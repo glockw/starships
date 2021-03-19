9f3718ab8b9cc7b5133e8ced3a75a7cc996b816a
Gabriel Guzman <gguzmanlockward@gmail.com>
2021-03-19 00:33:59 -0400

    adding value tu manufacturer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
-import {
-  FormControl,
-  InputLabel,
-  makeStyles,
-  MenuItem,
-  Select,
-} from "@material-ui/core";
-import { useContext } from "react";
 import "./App.css";
+import SelectForm from "./components/SelectForm";
 import Starchips from "./components/Starchips";
 import Layout from "./containers/Layout";
-import StarshipContextProvider, {
-  StarshipContext,
-} from "./context/StarshipContext";
-import { convertToCamelCase } from "./utils";
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
+import StarshipContextProvider from "./context/StarshipContext";
+
 function AppComponent() {
-  const { manufacturers, starships, changeManufacturer } = useContext(
-    StarshipContext
-  );
-
-  const classes = useStyles();
-  const change = (evt) => {
-    const { value: manufacturer } = evt.target;
-    changeManufacturer(manufacturer);
-  };
-
   return (
     <Layout>
-      <FormControl className={classes.formControl}>
-        <InputLabel> Manufacturers</InputLabel>
-        <Select onChange={change}>
-          {manufacturers.map((m) => (
-            <MenuItem key={convertToCamelCase(m)} value={m}>
-              {m}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Starchips data={starships} />
+      <SelectForm />
+      <Starchips />
     </Layout>
   );
 }
